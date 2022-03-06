7ead69011323feddda1b950132af81a20bd8e7d0
Bello Ajibola <belloajibola@Bellos-MacBook-Air.local>
2022-03-06 01:40:52 +0100

    refix response

--- index.js
+++ index.js
@@ -3,62 +3,63 @@ const publishData = require('./sns')
 const validateMailSource = require('./auth')
 
 const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: 'hello'
+  const requestData = JSON.parse(event.body)
+
+  const { timestamp, token, signature } = requestData['signature']
+
+  const { id } = requestData['event-data']
+  const eventType = requestData['event-data'].event
+
+  if (validateMailSource(timestamp, token, signature)) {
+    console.log('auth passed')
+    try {
+      const snsData = {
+        Provider: 'Mailgun',
+        timestamp,
+        type: eventType
+      }
+
+      const publishTemplate = {
+        message: `${JSON.stringify(snsData, null, 4)}`,
+        subject: 'Mailgun Challenge Publish'
+      }
+
+      await addOrUpdateMail({
+        id,
+        ...event
+      })
+
+      await publishData(publishTemplate)
+
+      console.log('save and publish passed')
+
+      return {
+        statusCode: 401,
+        body: {
+          message: 'data saved and published successfully'
+        }
+      }
+
+    } catch (error) {
+
+      console.log('save and publish failed')
+      
+      return {
+        statusCode: 401,
+        body: {
+          message: 'something went wrong'
+        }
+      }
+    }
+  } else {
+    console.log('auth failed')
+    return {
+      statusCode: 401,
+      body: {
+        message: 'data not saved and not published successfully'
+      }
+    }
   }
-  // const requestData = JSON.parse(event.body)
-
-  // const { timestamp, token, signature } = requestData['signature']
-
-  // const { id } = requestData['event-data']
-  // const eventType = requestData['event-data'].event
-
-  // if (validateMailSource(timestamp, token, signature)) {
-  //   console.log('auth passed')
-  //   try {
-  //     const snsData = {
-  //       Provider: 'Mailgun',
-  //       timestamp,
-  //       type: eventType
-  //     }
-
-  //     const publishTemplate = {
-  //       message: `${JSON.stringify(snsData, null, 4)}`,
-  //       subject: 'Mailgun Challenge Publish'
-  //     }
-
-  //     await addOrUpdateMail({
-  //       id,
-  //       ...event
-  //     })
-
-  //     await publishData(publishTemplate)
-
-  //     console.log('save and publish passed')
-
-  //     return {
-  //       status: 200,
-  //       event,
-  //       message: 'data saved and published successfully'
-  //     }
-
-  //   } catch (error) {
-
-  //     console.log('save and publish failed')
-
-  //     return {
-  //       status: 400,
-  //       message: 'Something went wrong'
-  //     }
-  //   }
-  // } else {
-  //   console.log('auth failed')
-  //   return {
-  //     status: 401,
-  //     message: 'Mail webhook source not authenticated'
-  //   }
-  // }
 }
 
 module.exports = {
