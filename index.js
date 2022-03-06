94adf44b2e5d0e2012a06a9b7390d18dc2b6a9b1
Bello Ajibola <belloajibola@Bellos-MacBook-Air.local>
2022-03-06 02:01:19 +0100

    stringfy message

--- index.js
+++ index.js
@@ -13,31 +13,31 @@ const handler = async (event) => {
   if (validateMailSource(timestamp, token, signature)) {
     console.log('auth passed')
     try {
-      // const snsData = {
-      //   Provider: 'Mailgun',
-      //   timestamp,
-      //   type: eventType
-      // }
+      const snsData = {
+        Provider: 'Mailgun',
+        timestamp,
+        type: eventType
+      }
 
-      // const publishTemplate = {
-      //   message: `${JSON.stringify(snsData, null, 4)}`,
-      //   subject: 'Mailgun Challenge Publish'
-      // }
+      const publishTemplate = {
+        message: `${JSON.stringify(snsData, null, 4)}`,
+        subject: 'Mailgun Challenge Publish'
+      }
 
-      // await addOrUpdateMail({
-      //   id,
-      //   ...event
-      // })
+      await addOrUpdateMail({
+        id,
+        ...event
+      })
 
-      // await publishData(publishTemplate)
+      await publishData(publishTemplate)
 
       console.log('save and publish passed')
 
-      console.log(requestData)
-
       return {
         statusCode: 200,
-        body: 'data saved and published successfully'
+        body: JSON.stringify({
+          message: 'data saved and published successfully'
+        })
       }
 
     } catch (error) {
@@ -46,7 +46,9 @@ const handler = async (event) => {
 
       return {
         statusCode: 401,
-        body: 'something went wrong'
+        body: JSON.stringify({
+          message: 'data not saved and not published successfully'
+        })
       }
     }
   } else {
@@ -54,7 +56,9 @@ const handler = async (event) => {
 
     return {
       statusCode: 401,
-      body: 'data not saved and not published successfully'
+      body: JSON.stringify({
+        message: 'auth faild'
+      })
     }
   }
 }
